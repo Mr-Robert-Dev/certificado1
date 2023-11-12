4fb2322791e66acf1787b601e216215375d1c128
Mr-Robert-Dev <146034010+Mr-Robert-Dev@users.noreply.github.com>
2023-11-11 22:07:19 -0300

    Update certificado.js

--- certificado.js
+++ certificado.js
@@ -3,6 +3,8 @@ let preencha = document.querySelector("input");
 let form = document.querySelector(".formulario")
 let certificado = document.querySelector(".certificate");
 let baixar = document.querySelector("#button")
+let baixar2 = document.querySelector("#button2")
+
 
 const nomeA =[]
 
@@ -13,14 +15,15 @@ document.getElementById("certificado").addEventListener("click", function () {
         nome.textContent = nomeA[0]; // Defina o valor de nome.textContent para o primeiro elemento do array
       }
       form.style.display = "none"
-      certificado.style.display = "flex"
       baixar.style.display ="flex"
+      certificado.style.display = "flex"
+
     });
 
 
 document.getElementById("button").addEventListener("click", function () {
     baixar.style.display ="none"
-
+    baixar2.style.display ="none"
     window.print();
 });
 
